70944f42cad11e9042c191c515a3988b31f0c840
abelcrvg <78155667+abelcrvg@users.noreply.github.com>
2026-09-05 17:45:42 -0300

    Fix reader style helper visibility

--- app/src/main/java/com/abelcrvg/newsrss/ReaderTextStyle.kt
+++ app/src/main/java/com/abelcrvg/newsrss/ReaderTextStyle.kt
@@ -4,7 +4,7 @@ import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 
 /** Local equivalent of Compose's withStyle helper, kept dependency-light. */
-private fun AnnotatedString.Builder.withStyle(style: SpanStyle, block: AnnotatedString.Builder.() -> Unit) {
+fun AnnotatedString.Builder.withStyle(style: SpanStyle, block: AnnotatedString.Builder.() -> Unit) {
     pushStyle(style)
     block()
     pop()
