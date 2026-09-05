c34fdd752e5db24ff52d26baaceefacaf000611e
abelcrvg <78155667+abelcrvg@users.noreply.github.com>
2026-09-04 23:46:26 -0300

    Add news source categories

---
+++ app/src/main/java/com/abelcrvg/newsrss/core/model/NewsCategory.kt
@@ -0,0 +1,12 @@
+package com.abelcrvg.newsrss.core.model
+
+enum class NewsCategory(val label: String) {
+    NEWS("Notícias"),
+    FOOTBALL("Futebol"),
+    TECHNOLOGY("Tecnologia"),
+    GAMES("Jogos"),
+    MOVIES("Filmes e séries"),
+    ECONOMY("Economia"),
+    SCIENCE("Ciência"),
+    WORLD("Mundo")
+}
