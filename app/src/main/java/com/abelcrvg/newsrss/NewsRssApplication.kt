6840e1ead4935dfd5d95a396041f7b350e044d0e
abelcrvg <78155667+abelcrvg@users.noreply.github.com>
2026-09-07 09:14:55 -0300

    Schedule background news refresh on app startup

--- app/src/main/java/com/abelcrvg/newsrss/NewsRssApplication.kt
+++ app/src/main/java/com/abelcrvg/newsrss/NewsRssApplication.kt
@@ -1,11 +1,13 @@
 package com.abelcrvg.newsrss
 
 import android.app.Application
+import com.abelcrvg.newsrss.data.background.NewsRefreshScheduler
 
 class NewsRssApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         appContext = this
+        NewsRefreshScheduler.schedule(this)
     }
 
     companion object {
